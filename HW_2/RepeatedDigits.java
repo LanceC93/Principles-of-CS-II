e698ba1a4468e36972de00d3ef3263790b96d3bf
LanceC93 <42276366+LanceC93@users.noreply.github.com>
2021-09-04 19:08:27 -0400

    finished RepeatDigits
    now prints occurrences of the digits and switched int to long for more possible digits

--- HW_2/RepeatedDigits.java
+++ HW_2/RepeatedDigits.java
@@ -1,5 +1,5 @@
 //Name: Lance Cross Date: 9/6/21
-//This program gets an int from the user and counts how many times each digit repeats
+//This program gets a number from the user and counts how many times each digit repeats
 import java.util.Scanner;
 
 public class RepeatedDigits {
@@ -7,18 +7,37 @@ public class RepeatedDigits {
         //gets int from user
         Scanner console = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int input = console.nextInt();
+        long input = console.nextLong();
 
-        printResults(input);
+        int[] occurences = countDigits(input);
+        printOccurences(occurences);
     }
-    //prints digits and their occurences
-    public static void printResults(int num) {
-        System.out.println("\nDigits:    0   1   2   3   4   5   6   7   8   9");
+    //prints the digits line and counts occurences
+    public static int[] countDigits(long num) {
+        System.out.println("\nDigits:         0    1    2    3    4    5    6    7    8    9");
         int[] occurences = {0,0,0,0,0,0,0,0,0,0};
         int lastDigit = 0;
-        for(int i = num; i > 0; i /= 10) {
-            lastDigit = i % 10;
+        for(long i = num; i > 0; i /= 10) {
+            lastDigit = (int)(i % 10);
             occurences[lastDigit] += 1;
         }
+        return occurences;
+    }
+    //prints digit occurences from array
+    public static void printOccurences(int[] occurences) {
+        String result = "Occurences: ";
+        for(int i = 0; i < occurences.length; i++) {
+            if(occurences[i] > 999) {
+                result = result + " ";
+            } else if(occurences[i] > 99) {
+                result = result + "  ";
+            } else if(occurences[i] > 9) {
+                result = result + "   ";
+            } else {
+                result = result + "    ";
+            }
+            result = result + occurences[i];
+        }
+        System.out.println(result);
     }
 }
