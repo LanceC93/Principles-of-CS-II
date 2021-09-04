fde73b4bf2bf6bd42555dd8055c6ffffe520fc47
LanceC93 <42276366+LanceC93@users.noreply.github.com>
2021-09-04 19:42:16 -0400

    finished IncomTax
    IncomeTax now displays the people who had the maximum tax values

--- HW_2/IncomeTax.java
+++ HW_2/IncomeTax.java
@@ -8,12 +8,17 @@ public class IncomeTax {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         //creates random int in the range 1-15
-        int randInt = (int)(Math.random() * 14) + 1;
-        displayTax(randInt, console);
+        int randInt = (int)((Math.random() * 14) + 1);
+        String maxPeople = displayTax(randInt, console);
+        printMaxes(maxPeople);
     }
-    //asks for income and displays amount of tax
-    public static void displayTax(int numPeople, Scanner console) {
+    //asks for income, displays amount of tax, and determines people with the maximum tax
+    public static String displayTax(int numPeople, Scanner console) {
         double tax = 0;
+        //tracks which numbered people have the max amount of tax
+        String maxPeople = "";
+        double maxTax = 0;
+
         //calculates taxes for incomes given by the user
         for(int i = 1; i <= numPeople; i++) {
             System.out.print("What is the income for person " + i + ": ");
@@ -30,7 +35,29 @@ public class IncomeTax {
             } else {
                 tax = 230 + ((.06 * tax) / 8000);
             }
+            if(tax > maxTax) {
+                maxTax = tax;
+                maxPeople = "" + i;
+            } else if(tax == maxTax) {
+                maxPeople = maxPeople + i;
+            }
             System.out.printf("The amount of tax for person %d is %.2f.\n", i, tax);
         }
+        return maxPeople;
+    }
+    //prints out people with the max amount of taxes
+    public static void printMaxes(String str) {
+        String result = "People ";
+        if(str.length() >= 3) {
+            for(int i = 0; i < str.length() - 1; i++) {
+                result = result + str.charAt(i) + ", ";
+            }
+            result = result + "and " + str.charAt(str.length() - 1) + " have ";
+        } else if(str.length() == 2) {
+            result = result + str.charAt(0) + " and " + str.charAt(1) + " have ";
+        } else {
+            result = "Person " + str + " has ";
+        }
+        System.out.println(result + "the max tax value.");
     }
 }
