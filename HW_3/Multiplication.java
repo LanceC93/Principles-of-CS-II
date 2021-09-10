73dd73556fa805d7793a974b217be2c0d2e1ac56
LanceC93 <42276366+LanceC93@users.noreply.github.com>
2021-09-10 19:10:00 -0400

    created recursive multiplication method

---
+++ HW_3/Multiplication.java
@@ -0,0 +1,15 @@
+//Name: Lance Cross Date: 9/13/21 ID: 002634761
+//This program contains a method named multiply that multiplys with just + and -
+//The main contains 3 examples of its execution
+public class Multiplication {
+    public static void main(String[] args) {
+        System.out.println(multiply(5,2));
+    }
+    public static int multiply(int a, int b) {
+        if(b != 1) {
+            a += multiply(a,b-1);
+        }
+        return a;
+
+    }
+}
