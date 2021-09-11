fea469a4305925dabb4abac68d1a7c406fd053d0
LanceC93 <42276366+LanceC93@users.noreply.github.com>
2021-09-10 21:31:25 -0400

    finished writeSquares method

--- HW_3/Squares.java
+++ HW_3/Squares.java
@@ -2,11 +2,21 @@
 //This program contains a method named writeSquares that prints even squares (below given integer n) in descending order
 //Then, it prints odd squares (below given integer n) in ascending order
 //The main contains 3 examples of its execution
+
 public class Squares {
     public static void main(String[] args) {
-        
+        writeSquares(4);
     }
+    //writes descending even squares and ascending odd squares below n
     public static void writeSquares(int n) {
-        
+        if(n == 1) {
+            System.out.print((int) Math.pow(n, 2));
+        } else if(n % 2 == 0) {
+            System.out.print((int) Math.pow(n, 2) + ", ");
+            writeSquares(n - 1);
+        } else {
+            writeSquares(n - 1);
+            System.out.print(", " + (int)Math.pow(n, 2));
+        }
     }
 }
