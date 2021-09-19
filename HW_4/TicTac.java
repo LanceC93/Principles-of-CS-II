bd919da31ab83e21d7a57408fb90e3865e2fcab1
LanceC93 <42276366+LanceC93@users.noreply.github.com>
2021-09-19 15:32:59 -0400

    created version with a 2d array instead of multiple arrays

--- HW_4/TicTac.java
+++ HW_4/TicTac.java
@@ -5,45 +5,33 @@ import java.util.Scanner;
 public class TicTac {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        char[] row1 = {'-', '-','-'};
-        char[] row2 = {'-', '-','-'};
-        char[] row3 = {'-', '-','-'};
+        char[][] board = {{'-', '-','-'}, {'-', '-','-'}, {'-', '-','-'}};
         int[] input = new int[2];
-        welcome(row1, row2, row3);
+        welcome(board);
         int turn = 0;
         do {
             turn ++;
             input = getInput(console);
-            if(input[0] == 0 && turn % 2 == 1) {
-                row1[input[1]] = 'x'; 
-            } else if(input[0] == 0 && turn % 2 == 0) {
-                row1[input[1]] = 'o';
-            } else if(input[0] == 1 && turn % 2 == 1) {
-                row2[input[1]] = 'x'; 
-            } else if(input[0] == 1 && turn % 2 == 0) {
-                row2[input[1]] = 'o';
-            } if(input[0] == 2 && turn % 2 == 1) {
-                row3[input[1]] = 'x'; 
-            } else if(input[0] == 2 && turn % 2 == 0) {
-                row3[input[1]] = 'o';
+            if(turn % 2 == 1) {
+                board[input[0]][input[1]] = 'x';
+            } else {
+                board[input[0]][input[1]] = 'o';
             }
-            printboard(row1, row2, row3);
-        } while(checkWinner(row1, row2, row3, turn));
+            printboard(board);
+        } while(checkWinner(board, turn));
     }
     //prints welcome message and the initial board
-    public static void welcome(char[] row1, char[] row2, char[] row3) {
+    public static void welcome(char[][] board) {
         System.out.println("Welcome to Tic-Tac-Toe. This is the board.");
-        printboard(row1, row2, row3);
+        printboard(board);
     }
     //prints the current state of the board
-    public static void printboard(char[] row1, char[] row2, char[] row3) {
+    public static void printboard(char[][] board) {
         System.out.println("-------------");
-        System.out.println("| " + row1[0] + " | " + row1[1] + " | " + row1[2] + " |");
-        System.out.println("-------------");
-        System.out.println("| " + row2[0] + " | " + row2[1] + " | " + row2[2] + " |");
-        System.out.println("-------------");
-        System.out.println("| " + row3[0] + " | " + row3[1] + " | " + row3[2] + " |");
-        System.out.println("-------------");
+        for(int i = 0; i <= 2; i++) {
+            System.out.println("| " + board[i][0] + " | " + board[i][1] + " | " + board[i][2] + " |");
+            System.out.println("-------------");
+        }
     }
     //this method gets input from the user
     public static int[] getInput(Scanner console) {
@@ -55,7 +43,7 @@ public class TicTac {
         return temp;
     }
     //checks the arrays to see if there is a winner
-    public static boolean checkWinner(char[] row1, char[] row2, char[] row3, int turn) {
+    public static boolean checkWinner(char[][] board, int turn) {
         return turn != 6;
     }
 }
