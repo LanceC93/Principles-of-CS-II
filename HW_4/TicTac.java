2d735361eba0e0e11784def9bf929953eafc16cf
LanceC93 <42276366+LanceC93@users.noreply.github.com>
2021-09-19 15:20:11 -0400

    finished creating a test checkwinner method

--- HW_4/TicTac.java
+++ HW_4/TicTac.java
@@ -8,7 +8,27 @@ public class TicTac {
         char[] row1 = {'-', '-','-'};
         char[] row2 = {'-', '-','-'};
         char[] row3 = {'-', '-','-'};
+        int[] input = new int[2];
         welcome(row1, row2, row3);
+        int turn = 0;
+        do {
+            turn ++;
+            input = getInput(console);
+            if(input[0] == 0 && turn % 2 == 1) {
+                row1[input[1]] = 'x'; 
+            } else if(input[0] == 0 && turn % 2 == 0) {
+                row1[input[1]] = 'o';
+            } else if(input[0] == 1 && turn % 2 == 1) {
+                row2[input[1]] = 'x'; 
+            } else if(input[0] == 1 && turn % 2 == 0) {
+                row2[input[1]] = 'o';
+            } if(input[0] == 2 && turn % 2 == 1) {
+                row3[input[1]] = 'x'; 
+            } else if(input[0] == 2 && turn % 2 == 0) {
+                row3[input[1]] = 'o';
+            }
+            printboard(row1, row2, row3);
+        } while(checkWinner(row1, row2, row3, turn));
     }
     //prints welcome message and the initial board
     public static void welcome(char[] row1, char[] row2, char[] row3) {
@@ -24,5 +44,18 @@ public class TicTac {
         System.out.println("-------------");
         System.out.println("| " + row3[0] + " | " + row3[1] + " | " + row3[2] + " |");
         System.out.println("-------------");
+    }
+    //this method gets input from the user
+    public static int[] getInput(Scanner console) {
+        int[] temp = new int[2];
+        System.out.print("Enter the row index:" );
+        temp[0] = console.nextInt();
+        System.out.print("Enter the column index:" );
+        temp[1] = console.nextInt();
+        return temp;
+    }
+    //checks the arrays to see if there is a winner
+    public static boolean checkWinner(char[] row1, char[] row2, char[] row3, int turn) {
+        return turn != 6;
     }
 }
