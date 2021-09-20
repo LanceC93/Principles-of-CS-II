14496d7716f8a217f32a3cc1bf0820630c176f15
LanceC93 <42276366+LanceC93@users.noreply.github.com>
2021-09-19 23:07:43 -0400

    finished Picture.java

--- HW_4/Picture.java
+++ HW_4/Picture.java
@@ -8,27 +8,39 @@ public class Picture {
         Graphics g = canvas.getGraphics();
         canvas.setBackground(Color.CYAN);
         createBox(0, 0, 100, 100, 5, 1, g, canvas);
+        createBox(10, 120, 24, 24, 4, 5, g, canvas);
+        createBox(150, 20, 40, 40, 5, 6, g, canvas);
+        createBox(130, 275, 36, 36, 3, 3, g, canvas);
     }
     //creates the box of circles with given properties
     public static void createBox(int x, int y, int width, int height, int circles, int numRows, Graphics g, DrawingPanel canvas) {
-        g.setColor(Color.GREEN);
-        g.fillRect(x, y, width, height);
+        int origX = x;
+        for(int i = 0; i < numRows; i++) {
+            for(int j = 0; j < numRows; j++) {
+                g.setColor(Color.GREEN);
+                g.fillRect(x, y, width, height);
 
-        g.setColor(Color.BLACK);
-        g.drawRect(x,y,width,height);
+                g.setColor(Color.BLACK);
+                g.drawRect(x,y,width,height);
 
-        g.setColor(Color.YELLOW);
-        g.fillOval(x, y, width, height);
+                g.setColor(Color.YELLOW);
+                g.fillOval(x, y, width, height);
 
-        g.setColor(Color.BLACK);
+                g.setColor(Color.BLACK);
 
-        for(int i = 0; i < circles; i++) {
-            g.drawOval(i *(width / (2*(circles))), i *(height / (2*(circles))), width - (width * i)/circles, height - (height * i)/circles);
+                for(int k = 0; k < circles; k++) {
+                    g.drawOval(x + k *(width / (2*(circles))), y + k *(height / (2*(circles))), width - (width * k)/circles, height - (height * k)/circles);
+                }
+
+                g.drawLine(x, y + (height/2), x + width, y + (height/2));
+                g.drawLine(x + (width/2), y, x + width/2, y + height);
+                g.drawLine(x,y,x + width,y + height);
+                g.drawLine(x + width,y,x,y + height);
+
+                x += width;
+            }
+            y += height;
+            x = origX;
         }
-
-        g.drawLine(0, height/2, height, height/2);
-        g.drawLine(width/2, 0, width/2, height);
-        g.drawLine(0,0,width,height);
-        g.drawLine(width,0,0,height);
     }
 }
